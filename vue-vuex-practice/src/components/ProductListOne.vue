afeb1f4154639b3bfb0e547e53667e04c3c5331c
King Fisher <tokingfisher19@gmail.com>
2019-04-20 17:46:46 +0600

    Lesson 01

---
+++ vue-vuex-practice/src/components/ProductListOne.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>Product List One</h2>
+        <ul>
+            <li v-for="(product, index) in products" :key="index">
+                <span class="name">{{ product.name }}</span>
+                <span class="price">{{ product.price }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['products'],
+    
+}
+</script >
+
+<style scoped>
+.name{
+        color: blueviolet;
+
+    }
+    .price{
+        color:chocolate;
+    }
+</style>
