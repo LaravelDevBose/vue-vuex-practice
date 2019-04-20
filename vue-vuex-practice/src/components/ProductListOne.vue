6ef7ba11749df5974f63e81c6c969b424692148e
King Fisher <tokingfisher19@gmail.com>
2019-04-20 17:55:19 +0600

    lesson design

--- vue-vuex-practice/src/components/ProductListOne.vue
+++ vue-vuex-practice/src/components/ProductListOne.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="product-list-one">
         <h2>Product List One</h2>
         <ul>
             <li v-for="(product, index) in products" :key="index">
@@ -18,11 +18,24 @@ export default {
 </script >
 
 <style scoped>
-.name{
-        color: blueviolet;
-
-    }
-    .price{
-        color:chocolate;
-    }
+#product-list-one{
+    background: #FFF8B1;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-one ul{
+    padding: 0;
+}
+#product-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #E8800C;
+}
 </style>
--- vue-vuex-practice/src/components/ProductListTwo.vue
+++ vue-vuex-practice/src/components/ProductListTwo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="product-list-two">
         <h2>Product List Two</h2>
         <ul>
             <li v-for="(product, index) in products" :key="index">
@@ -18,11 +18,25 @@ export default {
 </script>
 
 <style scoped>
-    .name{
-        color: red;
-
-    }
-    .price{
-        color: blueviolet;
-    }
+  #product-list-two{
+    background: #D1E4FF;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-two ul{
+    padding: 0;
+    list-style-type: none;
+}
+#product-list-two li{
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #860CE8;
+    display: block;
+}
 </style>
